0137fde722399c512b1df84e63779f7c86d23283
Levva01 <vellanifederico13@gmail.com>
2022-09-17 14:15:56 +0200

    function stampaArray completata

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿void StampaArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i == array.Length-1)
+        {
+            break;
+        }
+        else
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
