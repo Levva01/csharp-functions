550b5061564f58ca701ae600b762a195b3d0a4f6
Levva01 <vellanifederico13@gmail.com>
2022-09-17 14:38:22 +0200

    funzione sommaElementiArray completata

--- Program.cs
+++ Program.cs
@@ -20,3 +20,25 @@ int Quadrato(int numero)
 {   
     return numero *= numero;
 }
+
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] newArray = new int[array.Length];
+    array.CopyTo( newArray, 0 );
+
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Quadrato(newArray[i]);
+    }
+    return newArray;
+}
+
+int sommaElementiArray(int[] array)
+{
+    int somma = 0;
+    foreach (int n in array)
+    {
+        somma += n;
+    }
+    return somma;
+}
